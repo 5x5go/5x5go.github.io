6c02df21bc483b492357507b1f873b9de7d42ec4
5x5go <ashleyf@Ashleys-MacBook-Pro-3.local>
2016-03-26 15:55:42 -0700

    Intro link

--- pages/intro.js
+++ pages/intro.js
@@ -1,2 +1,2 @@
 showHeader();
-setDesc('&ldquo;Wood, slate and shell - circles, lines. From these simple elements a game of subtle and profound mysteries has evolved with popularity enduring over four-thousand years.&rdquo; <br /><br /><img width="300" height="216" src="img/slateshell.jpg" /><br /><br /><a href="#place?nocache">Learn on a 5&times;5 board</a>');
+setDesc('&ldquo;Wood, slate and shell - circles, lines. From these simple elements a game of subtle and profound mysteries has evolved with popularity enduring over four-thousand years.&rdquo; <br /><br /><img width="300" height="216" src="img/slateshell.jpg" /><br /><br /><a href="?place">Learn on a 5&times;5 board</a>');
